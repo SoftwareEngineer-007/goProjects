c5bce5f70be6f5cfae09e6289d40dcd7711aabd2
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-09-30 14:38:33 +0300

    errors

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	message, _ := enterTheClub(70)
+	message, err := enterTheClub(36)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	fmt.Println(message)
 	// fmt.Println(entered)
 }
 
-func enterTheClub(age int) (string, bool) {
+func enterTheClub(age int) (string, error) {
 	if age >= 18 && age < 45 {
-		return "Please Enter", true
+		return "Please Enter", nil
 	} else if age >= 45 && age < 65 {
-		return "Maybe you'll change your mind", true
+		return "Maybe you'll change your mind", nil
 	} else if age >= 65 {
-		return "You are so old", true
+		return "You are so old", errors.New("you are so old")
 	}
 
-	return "You are under 18 years old", false
+	return "You are under 18 years old", errors.New("you are so young")
 }
