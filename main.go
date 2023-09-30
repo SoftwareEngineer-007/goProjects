503806b556b3d79725cf19a5eece47ba2eee199f
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-09-30 12:36:28 +0300

    variables

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	message := "Hello, World!"
+	fmt.Println(message)
+
+	var number int = 16
+	var mess string = "End of program"
+	x := 15
+	y := 6
+	fmt.Println(number)
+	fmt.Println(x + y)
+	fmt.Println(number - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(mess)
 }
