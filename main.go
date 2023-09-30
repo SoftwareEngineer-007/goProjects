7ac9986370be89461bf90ca2f806200347b61a69
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-09-30 12:49:57 +0300

    variables

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ func main() {
 
 	var number int = 16
 	var mess string = "End of program"
+	var num float32 = 23.582
+	var a rune = 'S'
 	x := 15
 	y := 6
+
 	fmt.Println(number)
 	fmt.Println(x + y)
 	fmt.Println(number - y)
-	fmt.Println(x * y)
+	fmt.Println(num * float32(y))
 	fmt.Println(x / y)
+	fmt.Println(a)
 	fmt.Println(mess)
 }
