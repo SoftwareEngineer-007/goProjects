30c8a3d6276d9f976c06973f73993a8dfd035bba
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-09-30 14:27:22 +0300

    else if

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	message, _ := enterTheClub(16)
+	message, _ := enterTheClub(70)
 	fmt.Println(message)
 	// fmt.Println(entered)
 }
 
 func enterTheClub(age int) (string, bool) {
-	if age >= 18 {
+	if age >= 18 && age < 45 {
 		return "Please Enter", true
-	} else {
-		return "Entrance closed", false
+	} else if age >= 45 && age < 65 {
+		return "Maybe you'll change your mind", true
+	} else if age >= 65 {
+		return "You are so old", true
 	}
+
+	return "You are under 18 years old", false
 }
