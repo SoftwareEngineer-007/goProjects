e718b972be48bc6188246b4e295cff71115247a2
Alex Selyutin <selutin.alexandr90@gmail.com>
2023-09-30 15:55:24 +0300

    switch case

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	message, err := enterTheClub(36)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// message, err := enterTheClub(36)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	fmt.Println(message)
-	// fmt.Println(entered)
+	fmt.Println(prediction("thue"))
+	
 }
 
 func enterTheClub(age int) (string, error) {
@@ -26,4 +26,25 @@ func enterTheClub(age int) (string, error) {
 	}
 
 	return "You are under 18 years old", errors.New("you are so young")
+}
+
+func prediction(dayOfWeek string) (string, error) {
+	switch dayOfWeek {
+	case "sun":
+		return "Today is a day off. Do you work tomorrow!", nil
+	case "mon":
+		return "Have a good start to the week. Today is Monday", nil
+	case "tue":
+		return "Have a nice Tuesday", nil
+	case "wed":
+		return "Have a great Wednesday", nil
+	case "thu":
+		return "Have a wonderful Thursday", nil
+	case "fri":
+		return "End of the work week! Today is Friday", nil
+	case "sat":
+		return "You can relax. Today is Saturday", nil
+	default:
+		return "Enter the correct day of the week", errors.New("invalid day of week")
+	}
 }
